29dccc45e7bf426e3315caf8f8d480813a4506ff
MiguelQuinteiro <69766870+MiguelQuinteiro@users.noreply.github.com>
2026-02-28 18:48:00 -0300

    Simplify project structure creation in batch file
    Cambios para dejar un archivo vacio en la carpeta

--- crear_estructura.bat
+++ crear_estructura.bat
@@ -1,12 +1,10 @@
-
-@echo off
-echo Creando estructura de proyecto...
-
-mkdir src
 mkdir docs
+type nul > "docs\.gitkeep"
+
 mkdir examples
+type nul > "examples\.gitkeep"
+
 mkdir results
+type nul > "results\.gitkeep"
 
 echo Estructura creada correctamente.
-
-pause
