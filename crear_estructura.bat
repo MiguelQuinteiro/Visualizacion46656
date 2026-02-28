47de15297c9c99dd010b45b943e58a54d2ee35ca
MiguelQuinteiro <69766870+MiguelQuinteiro@users.noreply.github.com>
2026-02-28 19:00:21 -0300

    Refactor crear_estructura.bat to skip directory setup
    Removed directory creation and .gitkeep file creation.

--- crear_estructura.bat
+++ crear_estructura.bat
@@ -1,10 +1,3 @@
-mkdir docs
-type nul > "docs\.gitkeep"
-
-mkdir examples
-type nul > "examples\.gitkeep"
-
-mkdir results
 :: Movemos los archivos Index que ya existen a results
 move Index*.html results/
 
